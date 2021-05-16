49c76eac68b9c24023b2ad70ea2c075da4530868
Viz0616 <josevizcaya0616@gmail.com>
2021-05-16 18:13:56 -0400

    Updated database related stuff

--- app/models.py
+++ app/models.py
@@ -5,10 +5,12 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     id_document = db.Column(db.String(20), unique=True, nullable=False)
-    fee_comission = db.Column(db.Float, nullable=False, default=0.0)
-    fee_ticket = db.Column(db.Integer, db.ForeignKey('service.id'))
-    license_plate = db.relationship('Vehicle', backref='plate', uselist=False)
-    parking_usage = db.relationship('Usage', backref='parking_log', lazy=True)
+    #fee_comission = db.Column(db.Float, nullable=False, default=0.0)
+    #fee_ticket = db.Column(db.Integer, db.ForeignKey('service.id'))
+    license_plate = db.relationship(
+        'Vehicle', backref='plate', uselist=False)
+    parking_usage = db.relationship(
+        'Usage', backref='parking_log', lazy=True)
 
     def __repr__(self):
         return f"Person('{self.name}', '{self.id_document}')"
@@ -28,27 +30,30 @@ class Lot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     is_available = db.Column(db.Boolean, nullable=False)
     lot_type = db.Column(db.String(1), nullable=False)
-    vehicle_plate = db.relationship('Vehicle', backref='vehicle_lot', uselist=False)
+    vehicle_plate = db.relationship(
+        'Vehicle', backref='vehicle_lot', uselist=False)
 
     def __repr__(self):
         return f"Lot('{self.id}', '{self.lot_type}, '{self.is_available}')"
 
 
-class Service(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    client_id = db.relationship('Person', backref='client', uselist=False)
-    total_hours = db.Column(db.Integer, nullable=False)
-    total_fee = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f"Service('{self.id}', '{self.total_hours}','{self.total_fee}')"
+# class Service(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#
+#    total_hours = db.Column(db.Integer, nullable=False)
+#    total_fee = db.Column(db.Float, nullable=False)
+#
+#    def __repr__(self):
+#        return f"Service('{self.id}', '{self.total_hours}','{self.total_fee}')"
 
 
 class Usage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     in_time = db.Column(db.DateTime, nullable=False)
     out_time = db.Column(db.DateTime)
-    person_id_document = db.Column(db.Integer, db.ForeignKey('person.id_document'))
+    person_id_document = db.Column(
+        db.Integer, db.ForeignKey('person.id_document'))
+    total_charge = db.Column(db.Float, nullable=False, default=0.0)
 
     def __repr__(self):
         return f"Usage('{self.id}', '{self.in_time}', '{self.out_time}')"
--- app/utils.py
+++ app/utils.py
@@ -11,18 +11,18 @@ def is_full(lot_type):
     return len([lot for lot in Lot.query.filter_by(lot_type=lot_type) if not lot.is_available]) >= 24 if lot_type == "C" else len([lot for lot in Lot.query.filter_by(lot_type=lot_type) if not lot.is_available]) >= 6
 
 
-def total_charge(vehi_type, person_id_document):
+def total_charge(vehi_type, init_time, out_time):
     initial_charge = {"C": 0.50,
                       "M": 0.40}
     fee_per_hour = {"C": 0.40,
                     "M": 0.30}
 
-    recent_usage = Usage.query.filter_by(
-        person_id_document=perso_id_document).all()
-    hours = recent_usage[len(recent_usage) - 1].out_time - \
-        recent_usage[len(recent_usage) - 1].in_time
+    hours = out_time - init_time
 
-    return (initial_charge[vehi_type] * (int(round(hours.seconds / 3600)) * fee_per_hour[vehi_type]))
+    if int(round(hours.seconds / 3600)) == 0:
+        return initial_charge[vehi_type]
+    else:
+        return (initial_charge[vehi_type] * (int(round(hours.seconds / 3600)) * fee_per_hour[vehi_type]))
 
 
 
